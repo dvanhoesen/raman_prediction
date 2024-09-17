2062412794370d67371bf377be627cb719529339
daniel <danielvanhoesen@gmail.com>
2024-09-17 00:07:46 -0400

    iterating through spectrum and checking first and last values

--- extract_spectra.py
+++ extract_spectra.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
 import glob
+from brokenaxes import brokenaxes
+from matplotlib.patches import Rectangle
 
 
 basepath_raman = 'raman_data' + os.path.sep
@@ -33,11 +35,19 @@ x_components = []
 y_components = []
 
 files_failed = []
+files_exception = []
+files_problem_first_last = []
 
 x_mins = []
 x_maxs = []
 x_range = []
 x_resolution = []
+x_mins_idx = []
+x_maxs_idx = []
+x_first_value = []
+x_last_value = []
+x_val_diff = []
+x_first_two_diff = []
 
 count_file = 0
 
@@ -46,9 +56,9 @@ for file in all_files:
     y_temp = []
     
     try:
-        with open(file, 'r') as file:
+        with open(file, 'r', encoding='utf-8') as f:
        
-            for line in file:
+            for line in f:
                 # Strip any leading or trailing whitespace
                 line = line.strip()
                 
@@ -58,6 +68,9 @@ for file in all_files:
                 
                 elif line.startswith("##RRUFFID="):
                     rruffids.append(line.split('=')[1])
+
+                elif line.startswith("##END"):
+                    break
                 
                 # Skip other header lines
                 elif line.startswith("##"):
@@ -69,8 +82,9 @@ for file in all_files:
                     x_temp.append(float(data[0]))
                     y_temp.append(float(data[1]))
         
-    except:
+    except Exception as e:
         files_failed.append(file)
+        files_exception.append(e)
         count_file += 1
         continue
 
@@ -79,6 +93,47 @@ for file in all_files:
 
     min_val = np.amin(x_temp)
     max_val = np.amax(x_temp)
+
+    idx_min = np.argmin(x_temp)
+    idx_max = np.argmax(x_temp) - len(x_temp)
+
+    fv = x_temp[0]
+    lv = x_temp[-1]
+    vd = lv-fv
+    ftv = x_temp[1] - x_temp[0]
+
+    #if ftv > 10:
+    #    print(x_temp[0:5])
+    #    print(x_temp[-5:])
+    #    print(file)
+    #    files_problem_first_last.append(file)
+    
+    # The spectrum is decreasing in order
+    if vd < 0 or (idx_min > 1 or idx_max < -1):
+        #files_problem_first_last.append(file)
+        x_temp = x_temp[::-1]
+        y_temp = y_temp[::-1]
+
+        # recalculate the values
+        min_val = np.amin(x_temp)
+        max_val = np.amax(x_temp)
+
+        idx_min = np.argmin(x_temp)
+        idx_max = np.argmax(x_temp) - len(x_temp)
+
+        fv = x_temp[0]
+        lv = x_temp[-1]
+        vd = lv-fv
+        ftv = x_temp[1] - x_temp[0]
+    
+
+    if fv > 500:
+        files_problem_first_last.append(file)
+
+    if lv < 500 or lv > 3000:
+        files_problem_first_last.append(file)
+    
+
     range_val  = max_val - min_val
     resolution_val = len(x_temp) / range_val
 
@@ -86,6 +141,13 @@ for file in all_files:
     x_maxs.append(max_val)
     x_range.append(range_val)
     x_resolution.append(resolution_val)
+    x_mins_idx.append(idx_min)
+    x_maxs_idx.append(idx_max)
+
+    x_first_value.append(fv)
+    x_last_value.append(lv)
+    x_val_diff.append(vd)
+    x_first_two_diff.append(ftv)
 
     #x_components.append(x_temp)
     #y_components.append(y_temp)
@@ -104,9 +166,18 @@ rruffids_array = np.array(rruffids)
 #x_array = np.array(x_components, dtype=object)
 #y_array = np.array(y_components, dtype=object)
 
+
+
 print("Number of failed files: ", len(files_failed))
-for file in files_failed:
+for i in range(len(files_failed)):
+    print(files_failed[i])
+    print(files_exception[i])
+
+for file in files_problem_first_last:
     print(file)
+
+
+print("\nFIXING SOME FILES THAT HAVE PROBLEMS WITH THE FIRST AND LAST VALUES")
 
 
 ## Plot a histogram of the mins and maxs of x
@@ -114,13 +185,27 @@ x_mins = np.array(x_mins)
 x_maxs = np.array(x_maxs)
 x_range = np.array(x_range)
 x_resolution = np.array(x_resolution)
+x_mins_idx = np.array(x_mins_idx)
+x_maxs_idx = np.array(x_maxs_idx)
+x_first_value = np.array(x_first_value)
+x_last_value = np.array(x_last_value)
+x_val_diff = np.array(x_val_diff)
+x_first_two_diff = np.array(x_first_two_diff)
 
-bins_mins = np.linspace(0, 350, 50)
-bins_maxs = np.linspace(1000, 1600, 50)
-bins_range = np.linspace(1000, 1550, 50)
-bins_resolution = np.linspace(0.6, 2.1, 50)
+#bins_mins = np.linspace(0, 350, 50)
+#bins_maxs = np.linspace(1000, 1600, 50)
+#bins_range = np.linspace(1000, 1550, 50)
+#bins_resolution = np.linspace(0.6, 2.1, 50)
 
+bins_mins = 50
+bins_maxs = 50
+bins_range = 50
+bins_resolution = 50
+
+#fig, ax = plt.subplots(figsize=(9,5))
+#fig = plt.figure(figsize=(9, 5))
 fig, ax = plt.subplots(figsize=(9,5))
+#ax = brokenaxes(xlims=((-100, 500), (600, 7000)), hspace=.01)
 ax.hist(x_mins, bins=bins_mins, alpha=1, label='mins', color='blue')
 ax.hist(x_maxs, bins=bins_maxs, alpha=1, label='maxs', color='red')
 ax.set_xlabel('Wavenumber (1/cm)', fontsize=12)
@@ -139,11 +224,30 @@ ax2.set_xlabel('Points per Wavenumber', fontsize=12)
 ax2.set_ylabel('Spectrum Count', fontsize=12)
 ax2.legend(loc='best')
 
+#fig3, ax3 = plt.subplots(figsize=(9,5))
+#ax3.hist(x_mins_idx, bins=50, alpha=1, label='mins idx', color='blue')
+#ax3.hist(x_maxs_idx, bins=50, alpha=1, label='maxs idx', color='red')
+#ax3.set_xlabel('Index', fontsize=12)
+#ax3.set_ylabel('Spectrum Count', fontsize=12)
+#ax3.legend(loc='best')
+
+fig4, ax4 = plt.subplots(figsize=(9,5))
+ax4.hist(x_first_value, bins=50, alpha=1, label='first value', color='blue')
+ax4.hist(x_last_value, bins=50, alpha=1, label='last value', color='red')
+ax4.set_xlabel('First and Last Values', fontsize=12)
+ax4.set_ylabel('Spectrum Count', fontsize=12)
+ax4.legend(loc='best')
+
+fig5, ax5 = plt.subplots(figsize=(9,5))
+ax5.hist(x_val_diff, bins=50, alpha=1, color='blue')
+ax5.set_xlabel('Last Value - First Value', fontsize=12)
+ax5.set_ylabel('Spectrum Count', fontsize=12)
+
 
 print("Names Array:", names_array)
 print("RRUFFID Array:", rruffids_array)
 #print("X Components Array:", x_array)
 #print("Y Components Array:", y_array)
 
-
 plt.show()
+
